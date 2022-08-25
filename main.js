95835ecaafd42399ef112db3bc1d6bcce7a198e6
aimee0456 <112130419+aimee0456@users.noreply.github.com>
2022-08-25 16:55:29 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("Este es mi proyecto de consola");
+var nombre="Estela";
+var apellido=" Delgado";
+var nombre_completo= nombre.concat(apellido);
+console.log("Estela Delgado");
+console.log(nombre_completo);
